639a85f21ccf52e5ae665a0e405c99acb4ab1009
Vasyl Smutok <vasyl.smutok@gmail.com>
2022-10-18 17:51:53 +0300

    Solution, but sometimes an error occurs(

--- app/parse.py
+++ app/parse.py
@@ -3,6 +3,7 @@ import csv
 from dataclasses import dataclass, astuple
 from urllib.parse import urljoin
 from selenium import webdriver
+from selenium.common import ElementNotInteractableException
 from selenium.webdriver.common.by import By
 
 
@@ -14,9 +15,8 @@ TABLES_URL = urljoin(COMPUTERS_URL, "tablets")
 PHONES_URL = urljoin(HOME_URL, "phones/")
 TOUCH_URL = urljoin(PHONES_URL, "touch")
 
-PRODUCT_FIELDS = ["title", "description", "price", "rating", "num_of_reviews"]
-
-ALL_URLS = [HOME_URL, COMPUTERS_URL, LAPTOPS_URL,  TABLES_URL, PHONES_URL, TOUCH_URL]
+CSV_FILE_NAMES = ["home", "computers", "laptops", "tables", "phones", "touch"]
+ALL_URLS = [HOME_URL, COMPUTERS_URL, LAPTOPS_URL, TABLES_URL, PHONES_URL, TOUCH_URL]
 
 driver = webdriver.Chrome()
 
@@ -30,11 +30,15 @@ class Product:
     num_of_reviews: int
 
 
+PRODUCT_FIELDS = list(Product.__dict__["__annotations__"].keys())
+
+
 def parse_single_product(product_soup) -> Product:
     title = product_soup.find_element(By.CLASS_NAME, "title").text
-    print(title)
     description = product_soup.find_element(By.CLASS_NAME, "description").text
-    price = float(product_soup.find_element(By.CLASS_NAME, "price").text.replace("$", ""))
+    price = float(
+        product_soup.find_element(By.CLASS_NAME, "price").text.replace("$", "")
+    )
     rating = 2
     num_of_reviews = 2
 
@@ -47,19 +51,13 @@ def parse_single_product(product_soup) -> Product:
     )
 
 
-def get_single_page_product() -> list[Product]:
+def get_all_products_in_page() -> list[Product]:
     products = driver.find_elements(By.CLASS_NAME, "col-md-4")
     return [parse_single_product(product) for product in products]
 
 
-def get_all_products_in_page() -> list[Product]:
-    all_products = get_single_page_product()
-
-    return all_products
-
-
 def get_all_products():
-    for url in ALL_URLS:
+    for index, url in enumerate(ALL_URLS):
         driver.get(url)
 
         cookies = driver.find_elements(By.CLASS_NAME, "acceptCookies")
@@ -68,24 +66,22 @@ def get_all_products():
 
         more = driver.find_elements(By.XPATH, "/html/body/div[1]/div[3]/div/div[2]/a")
 
-        # try:
-        #     more[0]
-        # except:
-        #     more = []
-
         while more:
             if more[0].is_displayed():
                 more[0].click()
 
-            more = driver.find_elements(By.XPATH, "/html/body/div[1]/div[3]/div/div[2]/a")
+            more = driver.find_elements(
+                By.XPATH, "/html/body/div[1]/div[3]/div/div[2]/a"
+            )
+
             try:
                 more[0].click()
-            except:
-               more = None
+            except ElementNotInteractableException:
+                more = False
 
-        name_csv_file = url.split("/")[-2] if url.split("/")[-2] != "more" else "home"
+        name_csv_file = CSV_FILE_NAMES[index]
 
-        with open(f"../data/{name_csv_file}.csv", "w") as file:
+        with open(f"{name_csv_file}.csv", "w") as file:
             writer = csv.writer(file)
             writer.writerow(PRODUCT_FIELDS)
             products = get_all_products_in_page()
